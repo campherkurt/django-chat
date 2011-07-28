9acc618225e7d9090a98f4dd1d39bbe03a340a9c
Kurt Campher <campherkurt2@gmail.com>
2011-07-28 18:45:18 +0200

    Removed the virtual env files

--- build/test/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/kurtcampher/Projects/personal/xmpp/build/test/bin/python
-# EASY-INSTALL-SCRIPT: 'Django==1.0.2-final','django-admin.py'
-__requires__ = 'Django==1.0.2-final'
-import pkg_resources
-pkg_resources.run_script('Django==1.0.2-final', 'django-admin.py')
